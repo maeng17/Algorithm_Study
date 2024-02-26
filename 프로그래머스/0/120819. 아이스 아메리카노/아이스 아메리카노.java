2b96e573ef9413db882e2c13bafc966041b5b8a5
maeng17 <156390391+maeng17@users.noreply.github.com>
2024-02-26 22:47:35 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        while(money>=5500){
+            money -= 5500;
+            answer[0]++;
+        }
+        answer[1] = money;
+        return answer;
+    }
+}
