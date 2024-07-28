ef7f3274a2fc56ab900dddd75f8946948b9308ef
maeng17 <156390391+maeng17@users.noreply.github.com>
2024-07-28 16:37:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.74 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String ab = Integer.toString(a) + Integer.toString(b);
+        String ba = Integer.toString(b) + Integer.toString(a);
+        
+        answer = Integer.parseInt(ab) > Integer.parseInt(ba) ? Integer.parseInt(ab) : Integer.parseInt(ba);
+        return answer;
+    }
+}
