23d22aa9e14c33c3cbc4799cb5c3bd316caaeee2
maeng17 <156390391+maeng17@users.noreply.github.com>
2025-08-21 14:42:51 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int grade;
+
+        grade = sc.nextInt();
+
+        if(grade >= 90 ){
+            System.out.println("A");
+        } else if(grade>=80 && grade < 90){
+            System.out.println("B");
+        } else if(grade>=70 && grade < 80){
+            System.out.println("C");
+        } else if(grade>=60 && grade < 70){
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+    }
+}
