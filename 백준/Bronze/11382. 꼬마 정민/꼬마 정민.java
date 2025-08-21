ecaecefe1f1e117a466ea1e2bab9575a5a383c28
maeng17 <156390391+maeng17@users.noreply.github.com>
2025-08-21 14:22:48 +0900

    [Bronze V] Title: 꼬마 정민, Time: 184 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long A,B,C;
+        A = sc.nextLong();
+        B = sc.nextLong();
+        C = sc.nextLong();
+
+        System.out.println(A+B+C);
+        sc.close();
+
+    }
+}
