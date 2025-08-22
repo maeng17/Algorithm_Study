9c31e56bb32edc4e488e0d630042bf30ed6d0fba
maeng17 <156390391+maeng17@users.noreply.github.com>
2025-08-22 13:05:46 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int answer = 0;
+
+        for(int i=1; i<n+1; i++){
+            answer += i;
+
+        }
+        System.out.println(answer);
+        sc.close();
+    }
+}
