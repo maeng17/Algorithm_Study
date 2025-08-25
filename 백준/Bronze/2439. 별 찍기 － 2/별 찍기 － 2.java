7eb3ea717709389451f8e3f82327e245d9c921b5
maeng17 <156390391+maeng17@users.noreply.github.com>
2025-08-25 14:22:21 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T =Integer.parseInt(br.readLine()); //Scanner과 같은 역할. But 시간 단축
+
+        for(int i=1; i<=T; i++){
+          bw.append(" ".repeat(T-i));           
+          bw.append("*".repeat(i)); // repeat(n): n번 문자열 반복
+          bw.append("\n");
+        }
+
+        bw.flush(); //출력
+        bw.close(); //종료
+        br.close();
+
+    }
+}
